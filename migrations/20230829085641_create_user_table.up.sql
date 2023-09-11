dc5d28bf5bbb7c3418680ff7cec4f834062e7014
Anargyros Plemenos <anargyros.plemenos@sunlight.io>
2023-09-11 14:20:15 +0300

    Updated postgres docker image.

--- migrations/20230829085641_create_user_table.up.sql
+++ migrations/20230829085641_create_user_table.up.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS transactions (
     source_account_id uuid,
     target_account_id uuid,
     amount NUMERIC(8, 2),
-    currency TEXT,
+    currency TEXT
 );
 
 CREATE TABLE IF NOT EXISTS accounts (
     id uuid PRIMARY KEY,
     balance NUMERIC(8, 2),
     currency TEXT,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
